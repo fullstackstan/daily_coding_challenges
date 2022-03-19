090396f0211ce011d06d13eeb47d1ec7292e2658
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-03-19 16:07:20 -0500

    Create 3_19_2022.js

---
+++ 3_19_2022.js
@@ -0,0 +1,22 @@
+//8kyu Correct the mistakes of the character recognition software
+
+function correct(string)
+{
+let newString =''
+for (let i = 0;i<string.length;i++){
+  switch (string.charAt(i)){
+    case '5':
+    newString += 'S'
+    break
+    case '0':
+    newString += 'O'
+    break
+    case '1':
+    newString += 'I'
+    break
+    default: 
+    newString += string.charAt(i)
+    
+  }
+} return newString;
+}
