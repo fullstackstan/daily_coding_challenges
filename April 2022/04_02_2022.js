8eb47d1ce8ee12ae8bc9bbe9f7f84dd4f5555310
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-04-02 20:50:39 -0500

    solved 5kyu

--- April 2022/04_02_2022.js
+++ April 2022/04_02_2022.js
@@ -35,10 +35,43 @@ find the index of the second '.' if two '.' or first period if '/' and '.'
 get the string before that 
 */
 function domainName(url){
-    let indexOfFirstPeriodFound = url.indexOf('.')
-    console.log(indexOfFirstPeriodFound)
-    let secondPunctuation = url.lastIndexOf('.',indexOfFirstPeriodFound-1)
-    console.log(secondPunctuation)
+    let mySubString,firstPunctuation, secondPunctuation
+    if (url.includes('https://www')){
+         firstPunctuation = url.indexOf('.',13)
+         secondPunctuation = url.lastIndexOf('w.',firstPunctuation)
+         mySubString = url.substring(secondPunctuation+2,firstPunctuation)
+         console.log(mySubString)
+    }else
+    if (url.includes('http://www')){
+        firstPunctuation = url.indexOf('.',13)
+         secondPunctuation = url.lastIndexOf('w.',firstPunctuation)
+         mySubString = url.substring(secondPunctuation+2,firstPunctuation)
+         console.log(mySubString)
+    }else
+    if (url.includes('https://')){
+        firstPunctuation = url.indexOf('.')
+         secondPunctuation = url.lastIndexOf('/',firstPunctuation-1)
+         mySubString = url.substring(secondPunctuation+1,firstPunctuation)
+    }else
+    if (url.includes('http://')){
+         firstPunctuation = url.indexOf('.')
+         secondPunctuation = url.lastIndexOf('/',firstPunctuation-1)
+         mySubString = url.substring(secondPunctuation+1,firstPunctuation)
+    }else
+    if (url.includes('www')){
+        firstPunctuation = url.indexOf('.',5)
+         secondPunctuation = url.lastIndexOf('w.',firstPunctuation)
+         mySubString = url.substring(secondPunctuation+2,firstPunctuation)
+         console.log(mySubString)
+    } else{
+        firstPunctuation = url.indexOf('.')
+        mySubString = url.substring(0,firstPunctuation)
+        console.log(mySubString)
+    }
+    return mySubString
   }
   
-  domainName("http://google.com")
+  domainName("google.com")
+
+  /* this kata was on my mind all day and ifinally solved it
+  */
