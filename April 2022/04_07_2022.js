6260d8faa309ab6eacb72a2470f17eb97cc35687
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-04-07 17:55:59 -0500

    daily

---
+++ April 2022/04_07_2022.js
@@ -0,0 +1,12 @@
+// 6 kyu
+// Playing with digits
+
+function digPow(n, p){
+    //split apart numbers
+    let numberArray = n.toString().split('')
+    console.log(numberArray)
+    //mulitply number and put into array
+    let                 
+  }
+
+  digPow(89, 1)
