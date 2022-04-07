8cb295c0ec413a15acef923ea4e886e9b8577b6d
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-04-07 18:22:01 -0500

    solved

--- April 2022/04_07_2022.js
+++ April 2022/04_07_2022.js
@@ -6,7 +6,21 @@ function digPow(n, p){
     let numberArray = n.toString().split('')
     console.log(numberArray)
     //mulitply number and put into array
-    let                 
+    let multiplyArray=[]                
+    for (let i =0;i<numberArray.length;i++){
+        multiplyArray.push(numberArray[i]**(p+i))
+        console.log(multiplyArray)
+    }
+    let arraySum=multiplyArray.reduce((a,b)=>a+b)
+    console.log(arraySum)
+    let compareSum = 0
+    for (let k=1;compareSum<=arraySum;k++){
+        if (compareSum==arraySum){
+            return k-1
+        }
+        compareSum=n*k
+    } return -1
+    
   }
 
-  digPow(89, 1)
+  digPow(89444, 1)
