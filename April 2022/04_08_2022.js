426e6e0bfa60c98db802dfbef5a2d3b8d4ccd090
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-04-08 21:47:22 -0500

    the answer

--- April 2022/04_08_2022.js
+++ April 2022/04_08_2022.js
@@ -53,3 +53,12 @@ function towerBuilder(nFloors) {
   towerBuilder(6)
 
   ///i give up.  the family awaits.
+  function towerBuilder(nFloors) {
+    let tower = [];
+    
+    for (let i = 0; i < nFloors; i++) {
+      tower.push( ' '.repeat(nFloors-i-1) + '*'.repeat(i*2+1) + ' '.repeat(nFloors-i-1) );
+    }
+    
+    return tower;
+  }
