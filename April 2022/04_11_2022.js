8a151946fab522d769172c10264cc833455254ae
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-04-12 23:47:42 -0500

    nope

---
+++ April 2022/04_11_2022.js
@@ -0,0 +1,10 @@
+// 6 kyu
+// Help the bookseller !
+
+function stockList(listOfArt, listOfCat){
+    for (let element of c)    
+}
+
+let b = ["ABAR 200", "CDXE 500", "BKWR 250", "BTSQ 890", "DRTY 600"]
+let c = ["A", "B"]
+stockList(b, c)
