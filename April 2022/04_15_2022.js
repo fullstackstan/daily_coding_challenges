e8c706c666d489997695a2780e5ca9093e7d00ff
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-04-15 14:47:55 -0500

    updated solution

--- April 2022/04_15_2022.js
+++ April 2022/04_15_2022.js
@@ -4,6 +4,6 @@
 function squareSum(numbers){
     for (let i = 0;i<numbers.length;i++){
       numbers[i]*=numbers[i]
-    } if(numbers.length>0){return numbers.reduce((a,b)=>a+b)
-  } return 0
+    } return numbers.reduce((a,b)=>a+b,0)
+
     }
