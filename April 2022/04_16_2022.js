7638aef35e5f447f9025b515030023b92e4c4c87
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-04-16 19:45:41 -0500

    getting faster

---
+++ April 2022/04_16_2022.js
@@ -0,0 +1,26 @@
+// 6 kyu
+// Counting Duplicates
+
+function duplicateCount(text){
+    let counter=0
+    let stringMap = new Map()
+    let stringArray = text.split('')
+    for (let element of stringArray){
+        if(stringMap.has(element.toLowerCase())){
+            stringMap.set(element.toLowerCase(),(stringMap.get(element.toLowerCase())+1))
+        }else {
+            stringMap.set(element.toLowerCase(),1)
+        }
+    }console.log(stringMap)
+    let mapArray = stringMap.entries()
+    for (let element of stringMap.values()){
+        if(element>1){    
+        counter++
+        }
+    }
+       
+    return counter
+
+  }
+
+  duplicateCount("aaAAAAAAAbbcde")
