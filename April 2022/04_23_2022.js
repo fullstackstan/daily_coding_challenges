06c4367a347c960030f3d288439f675bc407b920
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-04-23 21:47:37 -0500

    easier

--- April 2022/04_23_2022.js
+++ April 2022/04_23_2022.js
@@ -1,3 +1,4 @@
+// 6kyu Counting Bits
 function countBits(n) {
     let binaryRep = n.toString(2)
     console.log(binaryRep)
@@ -9,3 +10,30 @@ function countBits(n) {
   
   
   };
+
+
+//   
+
+function findOutlier(integers){
+    let binaryArray=[]
+    let zeros = 0
+    let ones = 0
+    for (let i=0;i<integers.length;i++){
+        if(integers[i]<0){
+            binaryArray[i]=(integers[i]%2)*(-1)
+        } else {
+        binaryArray[i]=integers[i]%2
+        }
+        console.log(binaryArray[i])
+        if(binaryArray[i]==0){zeros++}
+        if(binaryArray[i]==1){ones++}
+    }
+    console.log(zeros,ones)
+    if(zeros>ones){
+        return integers[binaryArray.indexOf(1)]
+    } else{
+        return integers[binaryArray.indexOf(0)]
+    }
+}
+
+findOutlier([160, 3, 1719, 19, 11, 13, -21])
