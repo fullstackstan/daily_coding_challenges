dc0c09d14bebf7bf0b77ca9d22fb2efa3d33518b
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-04-26 08:58:20 -0500

    solved!

--- April 2022/04_25_2022.js
+++ April 2022/04_25_2022.js
@@ -1,19 +1,17 @@
 function persistence(num) {
-//split numtostring to array
-let numArray = []
-let result
-let counter = 0
-do {
-    numArray = num.toString().split('')
-    num = numArray.reduce((a,b)=>a*b,1)
-    counter++
-} while (num>9);
-
-console.log(num)
-return counter
-//multiply the elements
-
+    let numArray = []
+    let result
+    let counter = 0
+    if (num<=9){
+        return 0
+    }
+    while (num>9){
+        numArray = num.toString().split('')
+        num = numArray.reduce((a,b)=>a*b,1)
+        counter++
+    }
+    return counter
+    //split numtostring to array
 
 }
-
 persistence(39)
