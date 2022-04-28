5b07ffe6d3a38a5d1752ba6b6a53f5e0643f99e3
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-04-28 14:58:01 -0500

    almost done

--- April 2022/04_28_2022.js
+++ April 2022/04_28_2022.js
@@ -1,7 +1,10 @@
 function narcissistic(value) {
-   let arrayValue = value.toString().split('')
+   let arrayValue = Array.from(String(value),Number)
    console.log(arrayValue)
-   
+   let newArray = arrayValue.map(element=>element**arrayValue.length)
+   let sum = newArray.reduce((a,b)=>a+b,0)
+   console.log(sum)
+
   }
   
   narcissistic( 371 )
