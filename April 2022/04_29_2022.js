f309f3bc433db8aee8889b978cdbc8144e676bac
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-04-29 10:26:47 -0500

    pretty fast 5kyu

--- April 2022/04_29_2022.js
+++ April 2022/04_29_2022.js
@@ -1,3 +1,21 @@
+// 5 kyu
+// Moving Zeros To The End
+
 function moveZeros(arr) {
-    return
+   // find location of each zero
+   let zeroLocation = []
+   for(let i=0;i<arr.length;i++){
+    if(arr[i]===0){
+        zeroLocation.push(i)
+    }
+   }
+//reverse array of zero locations
+zeroLocation.reverse() 
+// splice and push using reveresed array
+for (let i=0;i<zeroLocation.length;i++){
+    let zeroToPush = arr.splice(zeroLocation[i],1)
+    arr.push(+zeroToPush)
+}
+return arr
+    
   }
