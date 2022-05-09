5af6ffcf23cc9a1b8f3c51743cc268fa25b93c1a
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-05-09 14:08:39 -0500

    easy 7

--- May 2022/05_09_2022.js
+++ May 2022/05_09_2022.js
@@ -6,3 +6,24 @@ function getAverage(marks){
     return Math.floor(studentAvg)
 }
 
+// 7 kyu
+// Disemvowel Trolls
+
+function disemvowel(str) {
+    //split apart the string to array
+    let strArray = str.split('')
+    let finalArray = []
+    //iterate through the array
+    for (let i=0;i<strArray.length;i++){
+        //push array to new final array
+        if(strArray[i].toLowerCase()=='a'||strArray[i].toLowerCase()=='e'||strArray[i].toLowerCase()=='i'||strArray[i].toLowerCase()=='o'||strArray[i].toLowerCase()=='u'){
+
+        } else{
+            finalArray.push(strArray[i])
+        }
+    // if its a vowel, dont push.
+    //return joined final array
+    }
+    
+    return finalArray.join('');
+  }
