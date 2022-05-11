2fe697afde227018aabbe77da9d7ac75ba99b6bb
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-05-11 13:42:31 -0500

    fast 6kyu

---
+++ May 2022/05_11_2022.js
@@ -0,0 +1,40 @@
+function solution (roman) {
+    //take in string and split to array
+    let romanArray = roman.split('')
+    //set map with roman numeral values
+    let romanMap = new Map()
+    romanMap.set('I',1)
+    romanMap.set('V',5)
+    romanMap.set('X',10)
+    romanMap.set('L',50)
+    romanMap.set('C',100)
+    romanMap.set('D',500)
+    romanMap.set('M',1000)
+    //create array of values 
+    let valueArray = []
+    let finalArray = []
+    for (let i=0;i<romanArray.length;i++){
+        valueArray[i]=romanMap.get(romanArray[i])
+    }
+    console.log(valueArray)
+    //iterate through
+  
+    for(let j=0;j<valueArray.length;j++)
+    //if (arr[i] <arr[i+1]){finalArray.push((arr[i+1]-arr[i]))}
+    if (valueArray[j] < valueArray[j+1]){
+        finalArray.push((valueArray[j+1]-valueArray[j]))
+        j++
+    } else {
+        finalArray.push(valueArray[j])
+    }
+    console.log(finalArray)
+    // complete the solution by transforming the 
+    // string roman numeral into an integer
+      return finalArray.reduce((a,b)=>a+b)
+  }
+
+  solution("MCMXC")
+
+  [1000, 100, 1000, 10, 100]
+  //iterate through
+  //if (arr[i] <arr[i+1]){finalArray.push((arr[i+1]-arr[i]))}
