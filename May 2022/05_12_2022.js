f7ef8f8e5425b01eecd28c361550ffc60277796f
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-05-12 11:14:58 -0500

    easy 8

--- May 2022/05_12_2022.js
+++ May 2022/05_12_2022.js
@@ -13,3 +13,32 @@ function digitize(n) {
   }
   digitize(35231)
  
+
+  //////
+
+//   8 kyu
+// Grasshopper - Grade book
+
+function getGrade (s1, s2, s3) {
+    //finds the average
+    let sum = s1+s2+s3
+    let average = sum/3
+    //find the letter grade
+    if (average>=90){
+        return 'A'
+    } else if (average>=80&&average<90){
+        return 'B'
+    } else if (average>=70&&average<80){
+        return 'C'
+    } else if (average>=60&&average<70){
+        return 'D'
+    } else {
+        return 'F'
+    }
+    //return the letter value
+  }
+  
+  parameters 3 inputs
+  return a letter grade
+  examples to the left
+  psuedo
