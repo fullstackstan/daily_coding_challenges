6a2e63dd1231bb1873a4dbe834ba69c6fb37ba02
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-05-13 18:39:43 -0500

    easy 8s

--- May 2022/05_13_2022.js
+++ May 2022/05_13_2022.js
@@ -2,3 +2,28 @@ function past(h, m, s){
     let millisec = (h*3.6e+6)+(m*60000)+(s*1000)
     return millisec
   }
+
+  function solution(str){
+    return str.split('').reverse().join('');  
+  }
+
+// 8 kyu
+// L1: Set Alarm
+  function setAlarm(employed, vacation){
+    if (employed==true&&vacation==false){
+      return true
+    } else {
+      return false
+    }
+    }
+    // 8 kyu
+    // Remove First and Last Character
+    function removeChar(str){
+        let array = str.split('')
+        array.pop()
+        array.shift()
+          return array.join('')
+        };
+        
+        
+        
