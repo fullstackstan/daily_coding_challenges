2c702292a5eceeb85216d5a1f6848089878d29b8
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-05-19 11:22:29 -0500

    tassks

---
+++ May 2022/05_18_2022.js
@@ -0,0 +1,31 @@
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+  return length*width*height  }
+  }
+
+  function greet(lang) {
+    let langs = {
+  english: 'Welcome',
+  czech: 'Vitejte',
+  danish: 'Velkomst',
+  dutch: 'Welkom',
+  estonian: 'Tere tulemast',
+  finnish: 'Tervetuloa',
+  flemish: 'Welgekomen',
+  french: 'Bienvenue',
+  german: 'Willkommen',
+  irish: 'Failte',
+  italian: 'Benvenuto',
+  latvian: 'Gaidits',
+  lithuanian: 'Laukiamas',
+  polish: 'Witamy',
+  spanish: 'Bienvenido',
+  swedish: 'Valkommen',
+  welsh: 'Croeso'
+    }
+    
+      return langs[lang]||langs.english
+    
+  }
+
+  
