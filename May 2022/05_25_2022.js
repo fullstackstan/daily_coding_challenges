42aeb350b7a64296503d9187100ae92e50920197
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-05-25 15:01:35 -0500

    tough 8

--- May 2022/05_25_2022.js
+++ May 2022/05_25_2022.js
@@ -1,23 +1,30 @@
-function greet (name, owner) {
+// function greet (name, owner) {
 
-    // Add code here
+//     // Add code here
     
-    return name==owner?'Hello boss':'Hello guest'
-  }
+//     return name==owner?'Hello boss':'Hello guest'
+//   }
   
 
 
-  ////  tough 7
-//   7 kyu
-//   Is this a triangle?
+//   ////  tough 7
+// //   7 kyu
+// //   Is this a triangle?
 
-  function isTriangle(a,b,c)
-{
-  if(a<0||b<0||c<0){return false}
-  //if((b>c>a)||(a>c>b)){return true}
-  if((b==c&&a<b)){
-    return true
-  }
-   return false;
-}
+//   function isTriangle(a,b,c)
+// {
+//   if(a<0||b<0||c<0){return false}
+//   //if((b>c>a)||(a>c>b)){return true}
+//   if((b==c&&a<b)){
+//     return true
+//   }
+//    return false;
+// }
 
+function howMuchILoveYou(nbPetals) {
+    //create array of words
+    let words = ['', 'I love you', 'a little', 'a lot', 'passionately', 'madly', 'not at all']
+    if (nbPetals<=6){return words[nbPetals]}
+ switch (nbPetals%6){
+     case 
+ }
