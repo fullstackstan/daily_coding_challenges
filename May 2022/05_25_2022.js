29691b16a73a367e5eea7d4a11131f0868e39682
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-05-25 12:12:25 -0500

    tough 7

---
+++ May 2022/05_25_2022.js
@@ -0,0 +1,22 @@
+function greet (name, owner) {
+
+    // Add code here
+    
+    return name==owner?'Hello boss':'Hello guest'
+  }
+  
+
+
+  ////  tough 7
+//   7 kyu
+//   Is this a triangle?
+
+  function isTriangle(a,b,c)
+{
+  if(a<0||b<0||c<0){return false}
+  //if((b>c>a)||(a>c>b)){return true}
+  if((b==c&&a<b)){
+    return true
+  }
+   return false;
+}
