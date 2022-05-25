e7e86e89474b12a3263cce4e5b3e696b1d409c92
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-05-24 23:33:08 -0500

    easy 6

---
+++ May 2022/05_24_2022.js
@@ -0,0 +1,13 @@
+function twoSum(numbersArray, target) {
+    // loop through array
+    for (let i=0;i<numbersArray.length;i++){
+        for (let j=numbersArray.length;j>0;j--){
+            if (numbersArray[i]+numbersArray[j]==target){
+                return [i,j]
+            }
+        }
+    }
+    //double loop through array
+    //if i+j == target
+    //return [i,j]
+  }
