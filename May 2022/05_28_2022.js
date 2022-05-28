1ddd4b96283ca088af1f633f1e265ea22b0e3e13
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-05-28 15:23:46 -0500

    easy 8

---
+++ May 2022/05_28_2022.js
@@ -0,0 +1,7 @@
+function lovefunc(flower1, flower2){
+    //if flower 1 is even and flower
+    if (flower1%2==0&&flower2%2==0){return false}
+    if (flower1%2!=0&&flower2%2!=0){return false}
+    return true
+  }
+  
