194dec7461d057da71b039e60738914ac36032bf
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-05-28 20:31:07 -0500

    easy5

--- May 2022/05_28_2022.js
+++ May 2022/05_28_2022.js
@@ -59,28 +59,51 @@ function Fighter(name, health, damagePerAttack) {
 //figure out who's first
 //2 loops fighter 1 first 1st loop
 
-// fighter 2 first 2nd loop
-//how the loops loops looks
-//fighter takes health
-//check health
-//other fighter takes health
-// check health
+// // fighter 2 first 2nd loop
+// //how the loops loops looks
+// //fighter takes health
+// //check health
+// //other fighter takes health
+// // check health
 
-function Fighter(name, health, damagePerAttack) {
-    this.name = name;
-    this.health = health;
-    this.damagePerAttack = damagePerAttack;
-    this.toString = function() { return this.name; }
-}
-function declareWinner(fighter1, fighter2, firstAttacker) {
-    //figure out who's first
-//2 loops fighter 1 first 1st loop
-    if(firstAttacker==fighter1.name){
-        for(let i= 0;fighter1.health>=0||fighter2.health>=0;i++){
-            fighter2.health = fighter2.health-fighter1.damagePerAttack
-            if(fighter2.health<=0){return fighter1.name}
-            fighter1.health = fighter1.health-fighter2.damagePerAttack
-            if(fighter1.health<=0){return fighter2.name}
+// function Fighter(name, health, damagePerAttack) {
+//     this.name = name;
+//     this.health = health;
+//     this.damagePerAttack = damagePerAttack;
+//     this.toString = function() { return this.name; }
+// }
+// function declareWinner(fighter1, fighter2, firstAttacker) {
+//     //figure out who's first
+// //2 loops fighter 1 first 1st loop
+//     if(firstAttacker==fighter1.name){
+//         for(let i= 0;fighter1.health>=0||fighter2.health>=0;i++){
+//             fighter2.health = fighter2.health-fighter1.damagePerAttack
+//             if(fighter2.health<=0){return fighter1.name}
+//             fighter1.health = fighter1.health-fighter2.damagePerAttack
+//             if(fighter1.health<=0){return fighter2.name}
+//         }
+//     }
+//   }
+
+var maxSequence = function(arr){
+    //declare sum and final sum tempSlice final slice
+    let tempSum = 0
+    let finalSum = 0
+    let tempSlice = []
+    // double loo
+    for(let i=0;arr.length>0;i++){
+        for(let j=0;j<arr.length;j++){
+            tempSlice = arr.slice(0,j+1)
+            tempSum = arr.reduce((a,b)=>a+b,0)
+            
         }
+        arr.shift()
     }
+    //first loop for  shift
+    //second loop for sum
+    //take slice from 0 to i
+    //if tempSum > final, final=temp and finalSlice = tempSlice
+    //return finalSlice
+  
   }
+  maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
