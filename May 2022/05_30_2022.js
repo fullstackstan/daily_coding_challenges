ac68ce71dc3f4279de62453be99ca67d6de04bf4
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-05-30 23:33:23 -0500

    nope

--- May 2022/05_30_2022.js
+++ May 2022/05_30_2022.js
@@ -1,11 +1,30 @@
 function queueTime(customers, n) {
     //while customers.leenght>0
+    if (n>customers.length){
+      n=customers.length
+    }
+    let totalTime =0
+    for (let i=0;i<customers.length;i++){
+      for (let j=0;j<n;j++){
+        customers[j]=customers[j]-1
+      }
+      
+      totalTime++
+      for (let k=0;k<customers.length;k++){
+        if(customers[k]==0){
+          customers.splice(k,1)
+          k--
+        }
+      }
+      i--
+    }
   //loop through array up until index<n-1
   //decrement each customer
   //increase total time
   //loop to splice all zero
 //if spice decrement counter
 
-//return totaltime
+return totalTime
 
   }
+  queueTime([2,2,3,3,4,4], 2)
