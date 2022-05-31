6fdc2d13ba3f1650b0f8bd87e87e3c416128c837
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-05-30 20:55:06 -0500

    psuedo

---
+++ May 2022/05_30_2022.js
@@ -0,0 +1,11 @@
+function queueTime(customers, n) {
+    //while customers.leenght>0
+  //loop through array up until index<n-1
+  //decrement each customer
+  //increase total time
+  //loop to splice all zero
+//if spice decrement counter
+
+//return totaltime
+
+  }
