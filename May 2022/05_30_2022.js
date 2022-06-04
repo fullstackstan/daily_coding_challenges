c2e8e0d07691ea1d696fc3f91e019d5d696f5c60
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-06-04 10:01:00 -0500

    done

--- May 2022/05_30_2022.js
+++ May 2022/05_30_2022.js
@@ -1,3 +1,11 @@
+<<<<<<< HEAD
+function feast(beast, dish) {
+    if((beast[0]==dish[0])&&(beast.charAt(beast.length-1)==dish.charAt(dish.length-1))){
+      return true   
+    }return false
+    }
+    
+
 function queueTime(customers, n) {
     //while customers.leenght>0
     if (n>customers.length){
@@ -28,3 +36,4 @@ return totalTime
 
   }
   queueTime([2,2,3,3,4,4], 2)
+>>>>>>> 5550ae3c3ed3e34128f19cc7fb71a87ebe34eced
