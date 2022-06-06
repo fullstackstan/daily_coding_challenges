9343d0545996aafb760bc0d853653673374a23e0
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-06-06 14:04:23 -0500

    yup

--- June 2022/06_05_2022.js
+++ June 2022/06_05_2022.js
@@ -5,3 +5,27 @@ function find_average(array) {
     avg=sum/array.length
     return avg
   }
+
+  function removeExclamationMarks(s) {
+    let array = []
+    for (let i = 0;i<s.length;i++){
+      if(s.codePointAt(i)!=33){
+        array.push(s[i])
+      }
+    }
+    return array.join('')
+  }
+
+  function rot13(message){
+      let holderArray=[]
+      for(let i=0;i<message.length;i++){
+          if ((message.codePointAt(i)>=97&&message.codePointAt(i)<=109)||(message.codePointAt(i)>=65&&message.codePointAt(i)<=77)){
+              holderArray.push(String.fromCodePoint(message.codePointAt(i)+13))
+          } else if ((message.codePointAt(i)>=110&&message.codePointAt(i)<=122)||(message.codePointAt(i)>=78&&message.codePointAt(i)<=90)){
+            holderArray.push(String.fromCodePoint(message.codePointAt(i)-13))
+          } else {
+              holderArray.push(message[i])
+          }
+      }
+      return holderArray.join('')
+  }
