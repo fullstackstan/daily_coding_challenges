b1ff99a3effd0bb6a6d4b7568693cbd4c639cb20
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-06-07 21:37:50 -0500

    tough

---
+++ June 2022/06_07_2022.js
@@ -0,0 +1,20 @@
+function longestConsec(strarr, k) {
+    // your code
+  //loop for while strarr.length>k
+  for(let i=0;strarr.length>k;i++){
+//loop through array
+for (;;){}
+  //concat first k elements
+  //push to holder array 
+  //shift strarr
+  //break
+  //let final array =[' ']
+  
+  //loop through holder
+  //if holder[i].length > final[0].length
+  
+  //return pop
+  }
+  
+  
+}
