3676456fd131c8f7867cbc7027488b484ea368d2
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-06-15 00:41:04 -0500

    old

--- June 2022/06_07_2022.js
+++ June 2022/06_07_2022.js
@@ -1,12 +1,19 @@
 function longestConsec(strarr, k) {
     // your code
+    let holderArray = []
+    let finalArray = [' ']
   //loop for while strarr.length>k
   for(let i=0;strarr.length>k;i++){
 //loop through array
-for (;;){}
-  //concat first k elements
+let concatArray = []
+for (let j=0;j<k;j++){
+    concatArray.push(strarr[j])
+    //concat first k elements
   //push to holder array 
+}
+  holderArray[i]=concatArray.join('')
   //shift strarr
+  strarr.shift()
   //break
   //let final array =[' ']
   
@@ -15,6 +22,13 @@ for (;;){}
   
   //return pop
   }
-  
+  //loop through holder
+  for (let l=0;l<holderArray.length;l++){
+      if (holderArray[l].length>finalArray[0].length){
+          finalArray[0]=holderArray[l]
+      }
+  }
+  //if holder[i].length > final[0].length
+  return finalArray
   
 }
