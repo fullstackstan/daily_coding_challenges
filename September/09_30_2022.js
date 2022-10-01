e4c89aa63b1d30990aceb45d446b4f13a572de66
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-09-30 22:38:58 -0500

    6 tougher than 5

--- September/09_30_2022.js
+++ September/09_30_2022.js
@@ -19,3 +19,42 @@ function validParentheses(parens) {
     //  parencounter++ or--
     //   return paren counter
     }
+
+    function validBraces(braces){
+        // split string
+          let braceArray = braces.split('')
+        // paren curly bracket
+          let paren=0, curly =0, bracket = 0
+        // loop through string
+          for (let i=0;i<braceArray.length;i++){
+            switch (braceArray[i]){
+                case '(':
+                if(braceArray[i+1]==='}'||braceArray[i+1]===']'){return false}
+                paren++
+                break
+                case '{':
+                if(braceArray[i+1]===')'||braceArray[i+1]===']'){return false}
+                curly++
+                break
+                case '[':
+                if(braceArray[i+1]==='}'||braceArray[i+1]===')'){return false}
+                bracket++
+                break
+                case ')':
+                paren--
+                break
+                case '}':
+                curly--
+                break
+                case ']':
+                bracket--
+                break
+                
+            }
+            if (paren===-1||curly===-1||bracket===-1){return false}
+          }
+          if (!paren&&!curly&&!bracket){return true}
+          return false
+        // switch
+        //   if any counter -1 return false
+        }
