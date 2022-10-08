197073a894c4a135fe73db5bb95ac6f0c61e633b
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-10-08 17:52:40 -0500

    day3 good

--- Huntober/Day3.js
+++ Huntober/Day3.js
@@ -1,1 +1,15 @@
-function removeKeys (str,keys)
+function removeKeys (str,keys){
+    keys = ['A','_','K','E','Y','!']
+    strArray = str.split('')
+for (let i=0;i<strArray.length;i++){
+    for (let j=0;j<keys.length;j++){
+        if (strArray[i]===keys[j]){
+            strArray[i]=""
+        }
+    }
+}
+console.log(strArray.join(''));
+}
+
+removeKeys('AyouEcould!thinkKthisAisYhard_toYreadKbeforeYreplacingEthe_keyYcharacters'
+)
