43952dfb6d206ebb337911cf25da87c458d1a6a9
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-10-08 22:38:24 -0500

    easy d4

---
+++ Huntober/Day4.js
@@ -0,0 +1,7 @@
+function reverseIt(str){
+    let strArray = str.split('')
+    return strArray.reverse().join('')
+}
+
+reverseIt(" .dne eht ta ecapsetihw eht hctam t'nseod gnirts siht fo gninnigeb eht ta ecaps etihw ehT   "
+)
