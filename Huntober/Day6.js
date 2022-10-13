c5199bdfc6fb5f3749391bbd9dfc8f819e4d5c68
Stanley Brown <97811857+fullstackstan@users.noreply.github.com>
2022-10-12 21:06:32 -0500

    easy day6

---
+++ Huntober/Day6.js
@@ -0,0 +1,19 @@
+function removeDecoy(str,count){
+    // str to array
+    let strArray=str.split('')
+    // create index array from count
+    let indexArray=[]
+    for (let i=0;i<strArray.length;i++){
+        indexArray.push((count*i)-1)
+    }
+    // reverse index array
+    indexArray.reverse()
+    // loop through splicing from behind
+    for (let i=0;i<indexArray.length;i++){
+        strArray.splice(indexArray[i],1)
+    }
+    // return joined array
+    return strArray.join('')
+}
+
+removeDecoy("The LBachyelor^ is ma tehrrib le tpelev!isioOn sh8ow.",5)
